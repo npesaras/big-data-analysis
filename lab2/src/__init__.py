436c5bec86f565c00ad52a14a6b52262185716a7
npesaras <nilmar.pesarast@gmail.com>
2025-11-30 20:27:07 +0800

    added src files

---
+++ lab2/src/__init__.py
@@ -0,0 +1,10 @@
+"""
+ITD105 - Big Data Analytics Lab Exercise #2
+Machine Learning Model Deployment Package
+
+This package provides separated data cleaning and model training
+functionality for diabetes classification and housing price regression.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Nilmar Pesaras"
